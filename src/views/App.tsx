eb7c7952b4385b01c27a5ee496ee3a74314ea317
niyongwei <1500920628@qq.com>
2021-08-26 17:38:28 +0800

    引入antd构建雏形

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,28 +1,40 @@
 import React from 'react'
 import { Redirect, Route, Switch, withRouter } from 'react-router-dom'
 import { Routers } from '@/router'
+import { Layout } from 'antd'
+const { Header, Footer, Sider, Content } = Layout
 import Top from '@/views/top/Top'
 function App() {
   return (
     <>
-      <Top />
-      <main>
-        <Switch>
-          {Routers.map((router) => (
-            <Route
-              exact={!router.notExect}
-              key={router.path}
-              path={router.path}
-              component={router.component}
-            ></Route>
-          ))}
-          {/* 设置默认路由 推荐方法一*/}
-          {/* 方法一 */}
-          {/* <Route path="/" component={Home} exact></Route> */}
-          {/* 方法二 重定向*/}
-          <Redirect path="/" to="/main" />
-        </Switch>
-      </main>
+      <Layout>
+        <Header>
+          <Top />
+        </Header>
+        <Layout>
+          <Content>
+            <main>
+              <Switch>
+                {Routers.map((router) => (
+                  <Route
+                    exact={!router.notExect}
+                    key={router.path}
+                    path={router.path}
+                    component={router.component}
+                  ></Route>
+                ))}
+                {/* 设置默认路由 推荐方法一*/}
+                {/* 方法一 */}
+                {/* <Route path="/" component={Home} exact></Route> */}
+                {/* 方法二 重定向*/}
+                <Redirect path="/" to="/main" />
+              </Switch>
+            </main>
+          </Content>
+          <Sider>Sider</Sider>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
     </>
   )
 }
