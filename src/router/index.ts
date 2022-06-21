e5a030b7339f7284841a423e1b95d32c34146af4
niyongwei <1500920628@qq.com>
2022-06-21 11:51:13 +0800

    router 2.0

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,51 +1,52 @@
-import {
-  Login,
-  Home,
-  Content,
-  Test2,
-  Test
-} from '@/views/router'
+// 路由懒加载
+import { lazy } from 'react'
+import renderRoutes from './renderRoutes'
+const Login = lazy(() => import('@/views/login'))
+const Home = lazy(() => import('@/views//home'))
+const Content = lazy(() => import('@/views/content'))
+const Test = lazy(() => import('@/views/Test'))
+const Test2 = lazy(() => import('@/views/Test2'))
 
-// export type RouterType = {
-//   path: string,
-//   component: React.LazyExoticComponent<any>,
-//   root: string[],
-//   notExect?: boolean,
-// }
+/* export type RouterType = {
+  path: string,
+  component: React.LazyExoticComponent<any>,
+  root: string[],
+  notExect?: boolean,
+}
 
-// const LoginRouter: RouterType = {
-//   path: '/Login',
-//   component: Login,
-//   root: [],
-// }
-// const HomeRouter: RouterType = {
-//   path: '/main',
-//   component: Home,
-//   root: [],
-// }
-// const ContentRouter: RouterType = {
-//   path: '/content',
-//   component: Content,
-//   root: [],
-// }
-// const TestRouter: RouterType = {
-//   path: '/test',
-//   component: Test,
-//   root: [],
-// }
-// const Test2Router: RouterType = {
-//   path: '/test/son',
-//   component: Test2,
-//   root: [],
-// }
-// // 总路由
-// const Routers: RouterType[] = ([
-//   LoginRouter,
-//   HomeRouter,
-//   ContentRouter,
-//   TestRouter,
-//   Test2Router
-// ])
+const LoginRouter: RouterType = {
+  path: '/Login',
+  component: Login,
+  root: [],
+}
+const HomeRouter: RouterType = {
+  path: '/main',
+  component: Home,
+  root: [],
+}
+const ContentRouter: RouterType = {
+  path: '/content',
+  component: Content,
+  root: [],
+}
+const TestRouter: RouterType = {
+  path: '/test',
+  component: Test,
+  root: [],
+}
+const Test2Router: RouterType = {
+  path: '/test/son',
+  component: Test2,
+  root: [],
+}
+// 总路由
+const Routers: RouterType[] = ([
+  LoginRouter,
+  HomeRouter,
+  ContentRouter,
+  TestRouter,
+  Test2Router
+]) */
 
 //编写基本的路由路线，path为路径，component为对应渲染的组件，exact属性决定是否精准匹配
 const routes = [
@@ -80,5 +81,6 @@ const routes = [
 ];
 
 export {
-  routes
+  routes,
+  renderRoutes
 }
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { Redirect, Route, Switch, withRouter, Link } from 'react-router-dom'
 import { routes } from '@/router'
-import { renderRoutes } from "react-router-config";
+// import { renderRoutes } from "react-router-config";
+import { renderRoutes } from '@/router'
 import { Layout } from 'antd'
 const { Header, Footer, Sider, Content } = Layout
-import  Head  from '@/common/components/Head/Head'
+import Head from '@/common/components/Head/Head'
+const authed = false // 如果登陆之后可以利用redux修改该值
+const authPath = '/login' // 默认未登录的时候返回的页面，可以自行设置
 function App() {
   return (
     <>
       <Layout>
         <Header>
-          <Head/>
+          <Head />
         </Header>
         <Layout>
           <Content>
-            <Link to='/test'>1</Link>
-            <Link to='/test/son'>2</Link>
+            <Link to="/test">1</Link>
+            <Link to="/test/son">2</Link>
             <main>
               {/* <Switch>标签会渲染将匹配URL的第一个<Route>进行渲染，如果最前面的<Route>匹配了，
               后面的就不会渲染，所以对于path="/"的<Route>应该放在最后面，<Switch>相当于js里的switch */}
@@ -29,12 +32,12 @@ function App() {
                   ></Route>
                 ))}
                 {/* 设置默认路由 推荐方法一*/}
-                {/* 方法一 */}
-                {/* <Route path="/" component={Home} exact></Route> */}
-                {/* 方法二 重定向*/}
-               {/* <Redirect path="/" to="/main" />
+              {/* 方法一 */}
+              {/* <Route path="/" component={Home} exact></Route> */}
+              {/* 方法二 重定向*/}
+              {/* <Redirect path="/" to="/main" />
               </Switch> */}
-              {renderRoutes(routes)}
+              {renderRoutes(routes, authed, authPath)}
             </main>
           </Content>
           <Sider>Sider</Sider>
